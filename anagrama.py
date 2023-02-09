78b1ae9c2ae754147265b35eda95e41b1f15c87f
bas-clavijo <bas.clavijo@duocuc.cl>
2023-02-09 18:09:44 -0300

    feat(anagrama):Edicion finalizada

--- anagrama.py
+++ anagrama.py
@@ -7,3 +7,12 @@ verdadero o falso (Bool) según sean o no anagramas.
 - NO hace falta comprobar que ambas palabras existan.
 - Dos palabras exactamente iguales no son anagrama.
 """
+#Un anagrama es algo que contiene las mismas palabras pero de forma desordenada
+#Un palíndromo es una palabra que se puede leer de la misma forma al derecho y al reves
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+        return False
+    return sorted(word_one.lower()) == sorted(word_two.lower())
+
+print(is_anagram("Amor", "Roma"))
